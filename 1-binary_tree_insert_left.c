f2036bf11308e8c1139b41d992b0961e41e5a70a
M'mah Zombo <www.mariamzombo@gmail.com>
2023-03-31 15:12:31 +0100

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,28 +7,32 @@
  * Return: pointer to the new node, or NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
     binary_tree_t *new_node;
-    
-    if (parent == NULL) {
+
+    if (parent == NULL)
+    {
         return (NULL);
     }
 
     new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL) {
+    if (new_node == NULL)
+    {
         return (NULL);
     }
 
-    if (parent->left == NULL) {
+    if (parent->left == NULL)
+    {
         parent->left = new_node;
     }
-    else {
+    else 
+    {
         new_node->left = parent->left;
         parent->left->parent = new_node;
         parent->left = new_node;
     }
 
     return (new_node);
-
 }
