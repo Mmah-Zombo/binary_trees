0c6f34b805a4d47604ec2a4013c8077ccf431dca
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-01 01:36:12 +0100

    task2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+
+	if (parent->right == NULL)
+	{
+		parent->right = new_node;
+	}
+	else
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+		parent->right = new_node;
+	}
+	return (new_node);
+}
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that creates a binary tree node
+ * @tree: pointer to the parent node of the node to delete
+ * Return: none
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+}
