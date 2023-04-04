4a996db8791eb761bdc09cd707610019aeb47423
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-04 12:07:41 +0100

    task 6.2

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preroder - goes through a binary tree using preorder
+ * binary_tree_preorder - goes through a binary tree using preorder
  * @tree: pointer to the root node of the tree
  * @func: pointer to the function to process the values of each node
  */
