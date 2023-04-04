07fc8e81bce94d727967b88bb178e4e1e2a20128
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-04 13:47:16 +0100

    task9.1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t leftsize, rightsize;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	leftsize = binary_tree_height(tree->left);
 	rightsize = binary_tree_height(tree->right);
