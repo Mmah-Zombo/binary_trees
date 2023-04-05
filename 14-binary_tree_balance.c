7aaa0a73aab37b6c7254d8d832517a7293e57b71
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-05 17:53:05 +0100

    task14.6

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	ln = binary_tree_height(tree->left);
-	rn = binary_tree_height(tree->right);
+	ln = tree_height(tree->left);
+	rn = tree_height(tree->right);
 
 	return (ln - rn);
+}
+
+/**
+ * tree_height - height of binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+	int height_left, height_right;
+
+	if (tree == NULL)
+		return (0);
+	height_left = tree_height(tree->left) + 1;
+	height_right = tree_height(tree->right) + 1;
+
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
 }
