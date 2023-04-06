bded1550e389f1db6a77e503e840a11eb43ef2e3
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-06 12:38:20 +0100

    task21.2

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -13,8 +13,8 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height2(tree->left);
+	right_height = binary_tree_height2(tree->right);
 
 	if (left_height == right_height)
 	{
@@ -33,20 +33,20 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - Calculates the height of a binary tree.
+ * binary_tree_height2 - Calculates the height of a binary tree.
  * @tree: A pointer to the root node of the tree to calculate the height.
  *
  * Return: The height of the tree.
  */
-int binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height2(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0;
 
 	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height2(tree->left);
+	right_height = binary_tree_height2(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
@@ -64,8 +64,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (1);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height2(tree->left);
+	right_height = binary_tree_height2(tree->right);
 
 	return (left_height == right_height && binary_tree_is_perfect(tree->left)
 		&& binary_tree_is_perfect(tree->right));
